46936519144f626ba21e5cec189e647531918576
lalosh <louayalosh17@gmail.com>
2021-01-25 14:24:07 +0200

    update build

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -5,6 +5,6 @@ interface Person {
 export declare function makeHttpRequest(): Promise<import("axios").AxiosResponse<any>>;
 export declare function makeImmutableChange(obj: any): <Base extends (newObj: any) => void>(base?: Base | undefined, ...rest: unknown[]) => any;
 export declare function useLodashToo(): import("lodash").Dictionary<string | undefined>;
-export declare function greenPerson(person: Person): void;
+export declare function greetPerson(person: Person): string;
 export declare const BUILD_TARGET: string | undefined;
 export {};
